6754ebe315095da2c14755f8b181d8fc14207d0e
amitmituu20 <67150240+amitmituu20@users.noreply.github.com>
2021-11-15 08:33:49 +0530

    Update settings.py

--- assignment2/settings.py
+++ assignment2/settings.py
@@ -122,6 +122,10 @@ STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
